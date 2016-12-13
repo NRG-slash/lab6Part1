2a2cbcc890f92ed849b287878f021fb852ca08c4
NRG-slash <Mclaturski@hotmail.com>
2016-12-13 10:41:06 -0600

    hvguisrhkswhgi
    waretgyh

---
+++ src/lab6Part1/lab6Part1.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package lab6Part1;
+
+/**
+*
+* Name: Laturski, Garrett William
+* Teacher: Mr.Hardman
+* Assignment #3
+* Date Last Modified: 
+*
+*/
+
+public class lab6Part1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		displayName();
+		displayBirthday();
+		displayFact();
+	}
+	/**
+	 * displayName displays my name
+	 * 
+	 * @param ther are no parameters
+	 * @return Nothing will be returned
+	 */
+	public static void displayName(){
+		
+		System.out.println("My name is Garrett William Laturski");
+		
+	}
+	
+	
+	/**
+	 * displayBirthday will show my birthday
+	 * 
+	 * @param ther are no parameters
+	 * @return Nothing will be returned
+	 */
+	public static void displayBirthday(){
+		
+		System.out.println("My birthday is August tenth, two thousand one(8/10/2001)");
+		
+	}
+	
+	
+	/**
+	 * displayFact will display a fact about me
+	 * 
+	 * @param there are no parameters
+	 * @return Nothing will be returned
+	 */
+	public static void displayFact(){
+		
+		System.out.println("My left Thumb is double-jointed");
+		
+	}
+
+}
